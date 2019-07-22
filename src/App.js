b43e8a5c266e877e5d70efeb88f1f3582448c5ed
Louell Sala <louell_sala@yahoo.com>
2019-07-22 15:07:41 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Medtechsolutions
+        </p>
+        <a
+          className="App-link"
+          href="https://medtechsolutions.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Emerging Healthcare Technology
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
