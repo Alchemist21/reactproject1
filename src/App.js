8dc0106d6f2e305cac352fb5c5d79022bce4c580
Louell Sala <louell_sala@yahoo.com>
2019-07-23 15:04:14 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
+
     <div className="App">
+    <>
+    
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        alt=""
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />
+      {' Home'}
+    </Navbar.Brand>
+  </Navbar>
+</>
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Medtechsolutions
+         Emerging Healthcare Technology
         </p>
         <a
           className="App-link"
@@ -16,7 +36,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Emerging Healthcare Technology
+         Medtechsolutions
         </a>
       </header>
     </div>
